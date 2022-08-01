f51e2800e07e31bc3ec134b83f2baae67493bde6
I <porteous@gmail.com>
2022-08-01 23:31:46 +0100

    add util scripts

---
+++ utils/curl.sh
@@ -0,0 +1,1 @@
+while true; do curl backend.rancher.localhost/version -m 0.1 --connect-timeout 0.1 && echo ; done
---
+++ utils/watch.sh
@@ -0,0 +1,1 @@
+watch -d -b -n 0.1 curl -s backend.rancher.localhost/version -m 0.1 --connect-timeout 0.1
