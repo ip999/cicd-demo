fbe5f8c346cc3807a67ee03e9e0d03b32480a1b4
I P <porteous@gmail.com>
2023-11-17 16:04:13 +0000

    bump yf

--- kev/main.py
+++ kev/main.py
@@ -5,7 +5,7 @@ import json
 from datetime import datetime
 import logging
 import sys
-
+import os
 
 
 # Logging setup
@@ -15,6 +15,7 @@ logging.basicConfig(stream=sys.stdout, level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(message)s')
 
 logging.info('Script started running.')
+logging.info(os.environ["DATABASE_URL"])
 
 
 # Constants
@@ -45,8 +46,7 @@ def init_db():
     cur.execute("""
         CREATE TABLE IF NOT EXISTS Vulnerabilities (
             VulnerabilityID SERIAL PRIMARY KEY,
-            MainID INTEGER,
-            REFERENCES Main(MainID),
+            MainID INTEGER REFERENCES Main(MainID),
             CVEID TEXT UNIQUE,
             VendorProject TEXT,
             Product TEXT,
@@ -65,8 +65,7 @@ def init_db():
     cur.execute("""
         CREATE TABLE IF NOT EXISTS Changes (
             ChangeID SERIAL PRIMARY KEY,
-            VulnerabilityID INTEGER,
-            REFERENCES Vulnerabilities(VulnerabilityID),
+            VulnerabilityID INTEGER REFERENCES Vulnerabilities(VulnerabilityID),
             ChangedDate TEXT,
             ChangedKey TEXT,
             OldValue TEXT,
@@ -99,17 +98,28 @@ def process_data(data):
 
     cur = conn.cursor()
 
+    logging.info("Preparing to insert data...")
+    logging.info(f"CatalogVersion: {data['catalogVersion']}")
+    logging.info(f"DateReleased: {data['dateReleased']}")
+    logging.info(f"Count: {data['count']}")
+    logging.info(f"JSONContent: {len(json.dumps(data))}")
+
+
     # Insert main data
     cur.execute("""
-        INSERT INTO Main (CatalogVersion, DateReleased, Count, JSONContent) VALUES (?, ?, ?, ?)
+        INSERT INTO Main (CatalogVersion, DateReleased, Count, JSONContent) VALUES (%s, %s, %s, %s)
     """, (data['catalogVersion'], data['dateReleased'], data['count'], json.dumps(data)))
+
+
+
+
     main_id = cur.lastrowid
 
     # Process vulnerabilities
     for vuln in data['vulnerabilities']:
         cve_id = vuln['cveID']
-        existing_vuln = cur.execute(
-            "SELECT * FROM Vulnerabilities WHERE CVEID = ?", (cve_id,)).fetchone()
+        cur.execute("SELECT * FROM Vulnerabilities WHERE CVEID = %s", (cve_id,))
+        existing_vuln = cur.fetchone()
 
         # If vulnerability exists, detect changes
         if existing_vuln:
@@ -117,7 +127,7 @@ def process_data(data):
                 # +2 to skip the initial columns (ID and MainID)
                 if str(existing_vuln[list(vuln.keys()).index(key) + 2]) != str(value):
                     cur.execute("""
-                        INSERT INTO Changes (VulnerabilityID, ChangedDate, ChangedKey, OldValue, NewValue) VALUES (?, ?, ?, ?, ?)
+                        INSERT INTO Changes (VulnerabilityID, ChangedDate, ChangedKey, OldValue, NewValue) VALUES (%s, %s, %s, %s, %s)
                     """, (existing_vuln[0], datetime.now(), key, existing_vuln[vuln.keys().index(key) + 2], value))
                     logging.info(f"Change detected for CVEID {cve_id}. Key: {key}. Old value: {existing_vuln[vuln.keys().index(key) + 2]}. New value: {value}.")
 
@@ -129,9 +139,9 @@ def process_data(data):
         # If new vulnerability, insert it
         else:
             columns = ", ".join(vuln.keys())
-            placeholders = ", ".join(["?" for _ in vuln.values()])
+            placeholders = ", ".join(["%s" for _ in vuln.values()])
             cur.execute(f"""
-                INSERT INTO Vulnerabilities (MainID, {columns}, DateStored) VALUES (?, {placeholders}, ?)
+                INSERT INTO Vulnerabilities (MainID, {columns}, DateStored) VALUES (%s, {placeholders}, %s)
             """, [main_id] + list(vuln.values()) + [datetime.now()])
 
     conn.commit()
